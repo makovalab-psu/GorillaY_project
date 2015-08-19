91d9f585a740dc7b229a43a0c1680c079012bf09
Samarth Rangavittal <szr165@brubeck.bx.psu.edu>
2015-08-19 16:01:07 -0400

    added Rplots

---
+++ RecoverY/plot_Threshold.R
@@ -0,0 +1,4 @@
+h1k <- read.delim("dsk_output/head1k_kmers", header=FALSE)
+jpeg("myplot.jpg")
+plot (h1k$V1, log(h1k$V2), xaxt="n", col="orange", ylab='Number of k-mers', xlab='Number of times a distinct k-mer occurs')
+dev.off()
